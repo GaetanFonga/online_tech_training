a2852c9388315f67b3665693826b9fef5dd97e2f
GaetanFonga <gaetanfonga@gmail.com>
2021-09-26 00:43:20 +0000

    [ADD] Fields Module

--- odoo_academy/models/course.py
+++ odoo_academy/models/course.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import UserError, ValidationError
 
 class Course(models.Model):
     
@@ -17,3 +18,21 @@ class Course(models.Model):
     
     active = fields.Boolean(string='Active', default=True)
     
+    base_price = fields.Boolean(string='Base Price', default=True)
+    
+    additional_fee = fields.Float(string='Additional fee', default=10.0)
+    
+    total_price = fields.Float(string='Total Price', readonly=True)
+    
+    @api.onchange('base_price', 'additional_fee')
+    def _onchange_total_price(self):
+        if self.base_price < 0.00:
+            raise UserError('Base Price cannot be set as Negative.')
+            
+        self.total_price = self.base_price + self.additionnal_fee
+    
+    @api.constrains('additional_fee')
+    def _chech_additional_fee(self):
+        for record in self:
+            if record.additional_fee < 10.00:
+                raise ValidationError('Additional fee cannot be less than 10.00: %s' % record.additional_fee)
